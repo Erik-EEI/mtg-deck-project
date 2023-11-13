d3ab506f40481cdcd68da868fd4ec6a55b4fe88f
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2023-11-13 11:42:36 +0100

    Add Backend project

---
+++ mtg-backend/src/main/java/com/codecool/mtgbackend/MtgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.mtgbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtgBackendApplication.class, args);
+    }
+
+}
