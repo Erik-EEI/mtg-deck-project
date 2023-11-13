39ea8bfcebc116a0850cb062341e5501c35aa290
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2023-11-13 13:01:06 +0100

    Set up react router with dummy home page for testing purposes

---
+++ mtg-frontend/src/Components/App.js
@@ -0,0 +1,26 @@
+import {
+  Navigate,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import '../index.css';
+import Layout from "./Layout";
+import { Home, ErrorPage} from "../Pages";
+
+const router = createBrowserRouter(createRoutesFromElements(
+    <Route errorElement={<ErrorPage />}>
+      <Route path={'/'} element={<Home />} >
+      <Route path={'/main'} element={<Layout />} />
+      </Route>
+    </Route>,
+));
+
+const App = () => {
+  return (
+      <RouterProvider router={router} />
+  );
+};
+
+export default App;
