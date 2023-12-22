9d36e6fda8a678e2be04b4be33f3ccf301bb5593
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2023-12-22 16:34:23 +0100

    extend displayed card info on modal component

--- mtg-frontend-react/src/Components/Discover/CardDetailModal/CardDetailModal.jsx
+++ mtg-frontend-react/src/Components/Discover/CardDetailModal/CardDetailModal.jsx
@@ -24,7 +24,25 @@ const CardDetailModal = ({ isModalVisible,handleOnKeyClose,handleOnClick,cardDat
         >
             <button className={'modal-close-button'} onClick={handleOnClick}>X</button>
             <div className={'card-modal-content'}>
-                <img src={cardData.image_uris.art_crop} />
+                <img src={cardData.image_uris.art_crop} className={'card-modal-art'} />
+                <h1>{cardData.name}</h1>
+                <div className={'card-modal-main-info'}>
+                    <div>
+                        <h3>MANA COST</h3>
+                        <h3>{cardData.mana_cost}</h3>
+                    </div>
+                    <div>
+                        <h3>CARD TYPE</h3>
+                        <h3>{cardData.type_line}</h3>
+                    </div>
+                    <div>
+                        <h3>COLOR</h3>
+                        <h3>{cardData.color_identity}</h3>
+                    </div>
+                </div>
+                <div className={'card-modal-sub-info'}>
+                    <p className={'card-data-modal-left'}>{cardData.oracle_text}</p>
+                </div>
             </div>
         </dialog>
     );
