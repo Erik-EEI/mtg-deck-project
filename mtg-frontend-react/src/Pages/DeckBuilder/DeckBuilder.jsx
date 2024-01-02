50a6e131e996caf86f2462b1a1ee9f5d534f1bd6
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2024-01-02 21:01:26 +0100

    Create the skeleton of the deck builder page

--- mtg-frontend-react/src/Pages/DeckBuilder/DeckBuilder.jsx
+++ mtg-frontend-react/src/Pages/DeckBuilder/DeckBuilder.jsx
@@ -1,9 +1,18 @@
 import './DeckBuilder.css';
+import {useParams} from "react-router-dom";
 
 const DeckBuilder = () => {
-    return (
-        <div>
+    const { name } = useParams();
 
+    return (
+        <div className={'deck-builder-page-container'}>
+            <h1 className={'deck-builder-deck-name'}>{name}</h1>
+            <div className={'deck-builder-search-container'}>SEARCH</div>
+            <div className={'deck-builder-result-container'}>RESULT</div>
+            <div className={'deck-builder-controls-container'}>
+                <button> SAVE </button>
+                <button> ERASE </button>
+            </div>
         </div>
     );
 };
