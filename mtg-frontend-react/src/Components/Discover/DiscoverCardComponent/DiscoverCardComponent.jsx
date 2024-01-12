c803a6d1522b146403c6fe4ee47fc5e891204690
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2024-01-12 19:53:35 +0100

    Resolve disappeared text on imageless cards in card component

--- mtg-frontend-react/src/Components/Discover/DiscoverCardComponent/DiscoverCardComponent.jsx
+++ mtg-frontend-react/src/Components/Discover/DiscoverCardComponent/DiscoverCardComponent.jsx
@@ -2,6 +2,7 @@ import './DiscoverCardComponent.css';
 import { useEffect } from "react";
 import {useImagePreloader} from "../../../Hooks/index.js";
 import {CardLoadingAnimation} from "../index.js";
+import {cardPlaceholder} from "../../../Assets/index.js";
 
 const DiscoverCardComponent = ({ cardData, onClick }) => {
     const { isLoading, src } = useImagePreloader( cardData, 'card' );
@@ -17,10 +18,13 @@ const DiscoverCardComponent = ({ cardData, onClick }) => {
                 :
                 <img
                 src={src}
-                className={'discover-card'}
+                className={ src === cardPlaceholder ? 'discover-card placeholder' : 'discover-card'}
                 onClick={() => onClick(cardData)}
                 alt={cardData.name}
                 />
+            }
+            {
+                src === cardPlaceholder && <p onClick={() => onClick(cardData)} className={'placeholder-text'}>{cardData?.name}</p>
             }
         </div>
     );
