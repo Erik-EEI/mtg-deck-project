dd850764ce59f37f7bfe63b0a85a22f14fabcea4
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2024-01-25 11:56:13 +0100

    Modify layout of deck viewer to include a controls container

--- mtg-frontend-react/src/Pages/DeckViewer/DeckViewer.jsx
+++ mtg-frontend-react/src/Pages/DeckViewer/DeckViewer.jsx
@@ -19,6 +19,9 @@ const DeckViewer = () => {
 
     return (
         <div className={'deck-viewer-container'}>
+            <section className={'deck-viewer-controls'}>
+
+            </section>
             <section className={'deck-viewer-stats-container'}>
 
             </section>
